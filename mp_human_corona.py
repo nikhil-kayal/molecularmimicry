bb591fa8c0c2b2c7d6b5bf22a6d1f04835e997de
nikhil-kayal <61706809+nikhil-kayal@users.noreply.github.com>
2020-06-10 12:53:44 -0400

    Add files via upload
    parallelized

--- mp_human_corona.py
+++ mp_human_corona.py
@@ -6,6 +6,17 @@ try:
 except ImportError:
     import pickle
       
+        
+with open("corona_id_to_seq.json", "r") as fp:
+    corona_id_to_seq = json.load(fp)
+with open("uniprot_dict.json", "r") as fp:
+    uniprot_dict = json.load(fp)
+with open("uniprot2gene.json", "r") as fp:
+    uniprot2gene = json.load(fp)
+with open("organism_to_keys.json", "r") as fp:
+    organism_to_keys = json.load(fp)
+with open("corona_id_to_protein.json", "r") as fp:
+    corona_id_to_protein = json.load(fp) 
     
 def longest_substring(s1, s2):
     t = [[0]*(1+len(s2)) for i in range(1+len(s1))]
@@ -27,55 +38,31 @@ def find_viral_species(key_to_find, dictionary):
         if key_to_find in value:
             return key
 
-def build_dump(corona_id_to_seq_dictionary):
-    
-    with open(os.getcwd() + "/uniprot_dict.json", "r") as fp:
-        uniprot_dict = json.load(fp)
-    with open(os.getcwd() + "/uniprot2gene.json", "r") as fp:
-        uniprot2gene = json.load(fp)
-    with open(os.getcwd() + "/organism_to_keys.json", "r") as fp:
-        organism_to_keys = json.load(fp)
-    with open(os.getcwd() + "/corona_id_to_protein.json", "r") as fp:
-        corona_id_to_protein = json.load(fp)
-    
-    for corona_id, corona_seq in corona_id_to_seq_dictionary.items():
-        print(corona_id)
-        for human_id, human_seq in uniprot_dict.items():
-            match = longest_substring(corona_seq, human_seq)
-            if match:
-                temp_dict = dict()
-                temp_dict["peptide"] = match
-                temp_dict["peptide_length"] = len(match)
-                temp_dict["human_protein"] = uniprot2gene[human_id]
-                temp_dict["human_protein_annotation"] = human_id
-                temp_dict["viral_species"] = find_viral_species(corona_id, organism_to_keys)
-                temp_dict["viral_protein"] = corona_id_to_protein[corona_id]
-                temp_dict["viral_protein_annotation"] = corona_id
-                temp_dict["human_protein_peptide_start"] = uniprot_dict[human_id].find(match)
-                temp_dict["human_protein_peptide_end"] = temp_dict["human_protein_peptide_start"] + temp_dict["peptide_length"] -1
-                temp_dict["viral_protein_peptide_start"] = corona_id_to_seq[corona_id].find(match)
-                temp_dict["viral_protein_peptide_end"] = temp_dict["viral_protein_peptide_start"] + temp_dict["peptide_length"] -1
-#                 temp_l.append(temp_dict)
-                
-    return temp_dict
-        
-    
-if __name__ == "__main__":
-    
+def build_dump(corona_id, corona_seq):
+    print(corona_id)
     temp_l = []
-    
-    with open(os.getcwd() + "/corona_id_to_seq.json", "rb") as fp:
-        corona_id_to_seq = json.load(fp)    
-       
-    for item in corona_id_to_seq.items():
-        manager = multiprocessing.Manager() # create only 1 mgr
-        d = manager.dict() # create only 1 dict
-        d = dict([item])
-        print(d, type(d))
-        p = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-        results = p.map(build_dump, d)
+    for human_id, human_seq in uniprot_dict.items():
+        match = longest_substring(corona_seq, human_seq)
+        if match:
+            temp_dict = dict()
+            temp_dict["peptide"] = match
+            temp_dict["peptide_length"] = len(match)
+            temp_dict["human_protein"] = uniprot2gene[human_id]
+            temp_dict["human_protein_annotation"] = human_id
+            temp_dict["viral_species"] = find_viral_species(corona_id, organism_to_keys)
+            temp_dict["viral_protein"] = corona_id_to_protein[corona_id]
+            temp_dict["viral_protein_annotation"] = corona_id
+            temp_dict["human_protein_peptide_start"] = uniprot_dict[human_id].find(match)
+            temp_dict["human_protein_peptide_end"] = temp_dict["human_protein_peptide_start"] + temp_dict["peptide_length"] -1
+            temp_dict["viral_protein_peptide_start"] = corona_id_to_seq[corona_id].find(match)
+            temp_dict["viral_protein_peptide_end"] = temp_dict["viral_protein_peptide_start"] + temp_dict["peptide_length"] -1
+            temp_l.append(temp_dict)
+    return temp_l
         
-    temp_l.append(results)
     
-    with open(os.getcwd() + "/corona_dump.p", "wb") as fp:
-        pickle.dump(temp_l, fp)
+pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+corona_id_to_seq_items = list(corona_id_to_seq.items())
+results = pool.starmap(build_dump, corona_id_to_seq_items)
+data = [ item for sublist in results for item in sublist ]
+with open("corona_dump.p", "wb") as fp:
+    pickle.dump(data, fp)
